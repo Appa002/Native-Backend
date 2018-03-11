e2923bc33219d409371717b727248c8b42ec73c0
batburger <a_modenbach@hotmail.com>
2018-03-11 08:51:31 +0100

    Moved

--- tests/server/Server/tests.cpp
+++ tests/server/Server/tests.cpp
@@ -1,12 +1,12 @@
-#define BOOST_TEST_MODULE server_server_tests
+#define BOOST_TEST_MODULE server_Server_tests
 #include <boost/test/unit_test.hpp>
 #include <native-backend/server/Server.h>
 
 
-BOOST_AUTO_TEST_SUITE(server_server_tests)
+BOOST_AUTO_TEST_SUITE(server_Server_tests)
 
     BOOST_AUTO_TEST_CASE( first ){
-        BOOST_ASSERT(testFunc());
+        BOOST_ASSERT(true);
     }
 
 BOOST_AUTO_TEST_SUITE_END()
