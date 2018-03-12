55b08bb59d11b811368bdfee217d15f030b4b7e0
batburger <a_modenbach@hotmail.com>
2018-03-12 20:25:28 +0100

    Added skeleton for test case

--- tests/server/Server/tests.cpp
+++ tests/server/Server/tests.cpp
@@ -1,12 +1,24 @@
 #define BOOST_TEST_MODULE server_Server_tests
+
 #include <boost/test/unit_test.hpp>
 #include <native-backend/server/Server.h>
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <boost/asio.hpp>
+#include <boost/array.hpp>
+#include <boost/thread/thread.hpp>
 
+using boost::asio::ip::tcp;
 
 BOOST_AUTO_TEST_SUITE(server_Server_tests)
 
-    BOOST_AUTO_TEST_CASE( first ){
-        BOOST_ASSERT(true);
+    BOOST_AUTO_TEST_CASE(server_existents_check) {
+        boost::thread t([]() {
+            native_backend::Server::create(8080);
+        });
+        /*TODO: Find way to make a tcp request and check the servers response. Similar to how netcat works in bash*/
+        t.interrupt();
     }
 
 BOOST_AUTO_TEST_SUITE_END()
