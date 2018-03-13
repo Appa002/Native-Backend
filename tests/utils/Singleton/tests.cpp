204d4808b21e0ffa91a721afff754abecab4c554
batburger <a_modenbach@hotmail.com>
2018-03-13 21:14:19 +0100

    Moved

--- tests/utils/Singleton/tests.cpp
+++ tests/utils/Singleton/tests.cpp
@@ -1,9 +1,9 @@
-#define BOOST_TEST_MODULE routing_Root_tests
+#define BOOST_TEST_MODULE utils_Singleton_tests
 #include <boost/test/unit_test.hpp>
 #include <native-backend/parser/parser.h>
 
 
-BOOST_AUTO_TEST_SUITE(routing_Root_tests)
+BOOST_AUTO_TEST_SUITE(utils_Singleton_tests)
 
 BOOST_AUTO_TEST_CASE( first ){
     BOOST_ASSERT(true);
