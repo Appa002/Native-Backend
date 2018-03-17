88ffde70004d0bf481032d9602631c7bc3f5e04f
batburger <a_modenbach@hotmail.com>
2018-03-17 13:13:15 +0100

    Added Basic widgets to display something

--- tests/server/Server/tests.cpp
+++ tests/server/Server/tests.cpp
@@ -2,23 +2,34 @@
 
 #include <boost/test/unit_test.hpp>
 #include <native-backend/server/Server.h>
-#include <ctime>
-#include <iostream>
-#include <string>
-#include <boost/asio.hpp>
 #include <boost/array.hpp>
 #include <boost/thread/thread.hpp>
+#include <native-backend/routing/Router.h>
+#include <native-backend/widgets/IWidget.h>
+#include <boost/move/unique_ptr.hpp>
+#include <native-backend/widgets/TextWidget.h>
+#include <native-backend/widgets/BlockWidget.h>
 
 using boost::asio::ip::tcp;
 
 BOOST_AUTO_TEST_SUITE(server_Server_tests)
 
     BOOST_AUTO_TEST_CASE(server_existents_check) {
+
+        nvb::Router::getInstance()->addRoute(nvb::HttpVerb::Verb::get, "/", [](){
+            return boost::movelib::unique_ptr<nvb::IWidget>(
+                    new nvb::BlockWidget(
+                            new nvb::TextWidget("Hello, World!")
+                          //  new nvb::BlockWidgetStyle(nvb::BlockWidgetStyle::PositionHint::CENTER, nvb::BlockWidgetStyle::PositionHint::TOP)
+                    ));
+        });
+
         boost::thread t([]() {
             nvb::Server::create(8080);
         });
         /*TODO: Find way to make a tcp request and check the servers response. Similar to how netcat works in bash*/
-        t.interrupt();
+       // t.interrupt();
+        t.join();
     }
 
 BOOST_AUTO_TEST_SUITE_END()
