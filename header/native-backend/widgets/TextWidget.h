70ff4d02a3630dc1197603696f3a2a0b0572f2a1
batburger <a_modenbach@hotmail.com>
2018-03-17 13:12:31 +0100

    Added Styling

--- header/native-backend/widgets/TextWidget.h
+++ header/native-backend/widgets/TextWidget.h
@@ -12,20 +12,28 @@ namespace nvb{
     /*!\brief Basic Widget which takes no children and shows simple text on screen.*/
     class TextWidget : public IWidget {
     private:
-        const std::string raw_html;
-
+        const std::string raw_html = "<span style=\"white-space: nowrap\">[INSERT]</span>";
+        const std::string text;
     public:
-        explicit TextWidget(std::string& text) : raw_html("<p>"+text+"</p>"){};
-        explicit TextWidget(std::string&& text) : raw_html("<p>"+text+"</p>"){};
+        explicit TextWidget(std::string& text) : text(text) {};
+        explicit TextWidget(std::string&& text) : text(text) {};
 
         /*!\brief Injects \c TextWidget::raw_html into the document at position \c pos.*/
         std::string build(std::string& document, size_t pos) override {
-            return document.insert(pos, raw_html);
+            size_t pos_change = raw_html.find_first_of("[INSERT]");
+            std::string insert_html = raw_html;
+            insert_html = insert_html.replace(pos_change, 8, "");
+            insert_html = insert_html.insert(pos_change, text);
+            return document.insert(pos, insert_html);
         }
 
         /*!\brief Injects \c TextWidget::raw_html into the document at position \c pos.*/
         std::string build(std::string&& document, size_t pos) override {
-            return document.insert(pos, raw_html);
+            size_t pos_change = raw_html.find_first_of("[INSERT]");
+            std::string insert_html = raw_html;
+            insert_html = insert_html.replace(pos_change, 8, "");
+            insert_html = insert_html.insert(pos_change, text);
+            return document.insert(pos, insert_html);
         }
     };
 }
