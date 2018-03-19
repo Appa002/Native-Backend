ed18474dce2571a77a48bc3b31e0bb1c3d48fc92
batburger <a_modenbach@hotmail.com>
2018-03-19 18:46:36 +0100

    Changed to use boost::shared_ptr instead of boost::movelib::unique_ptr

--- header/native-backend/routing/Router.h
+++ header/native-backend/routing/Router.h
@@ -11,20 +11,21 @@
 #include <native-backend/widgets/IWidget.h>
 #include <unordered_map>
 #include <boost/move/unique_ptr.hpp>
+#include <boost/shared_ptr.hpp>
 
 namespace nvb{
     /*!\brief Used to map functions to certain http requests.*/
     class Router : public Singleton<Router> {
     public:
         Router() = default;
-        void addRoute(HttpVerb::Verb verb, std::string path, std::function<boost::movelib::unique_ptr<IWidget>(void)> builder);
-        boost::movelib::unique_ptr<IWidget> evaluateRoute(HttpVerb::Verb verb, std::string path);
+        void addRoute(HttpVerb::Verb verb, std::string path, std::function<boost::shared_ptr<IWidget>(void)> builder);
+        boost::shared_ptr<IWidget> evaluateRoute(HttpVerb::Verb verb, std::string path);
     private:
         /*!\brief Member for saving routes.
         Example usage: The path is '/user/index', the http verb is get
          routes_map_.find('/user/index').find(HttpVerb::get) would return the registered function.*/
         std::unordered_map<std::string,
-                std::unordered_map<HttpVerb::Verb, std::function<boost::movelib::unique_ptr<IWidget>(void)>>> routes_map_;
+                std::unordered_map<HttpVerb::Verb, std::function<boost::shared_ptr<IWidget>(void)>>> routes_map_;
     };
 }
 
