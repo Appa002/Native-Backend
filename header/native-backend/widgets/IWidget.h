2c038e094b645035e93547605c765d070b3a8f9b
batburger <a_modenbach@hotmail.com>
2018-03-19 18:45:58 +0100

    Optimized code to only create the HTML code only during initialization.

--- header/native-backend/widgets/IWidget.h
+++ header/native-backend/widgets/IWidget.h
@@ -7,15 +7,22 @@
 
 #include <boost/move/unique_ptr.hpp>
 #include <string>
+#include <boost/shared_ptr.hpp>
 
 namespace nvb{
-    /*!\brief Interface from which all Widgets inherits.
+    /*!\brief Interface from which all Widgets inherit.
      * The \c build method is used by all Widgets to inject there html code into the document.*/
     class IWidget {
     public:
         virtual ~IWidget() = default;
+
+        /*!\brief Injects the HTML code of the widget into the document at a given position.*/
         virtual std::string build(std::string&, size_t) = 0;
+        /*!\brief Injects the HTML code of the widget into the document at a given position.*/
         virtual std::string build(std::string&&, size_t) = 0;
+        /*!\brief Returns the size of the string which is going to be injected into the document.
+         * This size will also include the HTML code of all children.*/
+        virtual size_t contentSize() = 0;
     };
 }
 
