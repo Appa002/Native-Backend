a372a56218d83679b21abd3e4f5a1fc98a32aeb3
batburger <a_modenbach@hotmail.com>
2018-03-19 18:47:11 +0100

    Removed deprecated widget

--- tests/widget/Widget/tests.cpp
+++ tests/widget/Widget/tests.cpp
@@ -1,16 +1,16 @@
 #define BOOST_TEST_MODULE widget_Widget_tests
 #include <boost/test/unit_test.hpp>
-#include <native-backend/widgets/DivWidget.h>
 #include <native-backend/widgets/TextWidget.h>
 
 BOOST_AUTO_TEST_SUITE(widget_Widget_tests)
 
 BOOST_AUTO_TEST_CASE( basic_interface_test ){
-        auto w = boost::movelib::unique_ptr<nvb::DivWidget>(new nvb::DivWidget(
-                new nvb::TextWidget("Hello, World!")));
+        auto w = boost::movelib::unique_ptr<nvb::IWidget>(
+                new nvb::TextWidget("Hello, World!")
+        );
 
         std::string out = w->build("", 0);
-        std::string expected = "<div><p>Hello, World!</p></div>";
+        std::string expected = R"(<span style="white-space: nowrap">Hello, World!</span>)";
         BOOST_CHECK_EQUAL(out, expected);
     }
 
