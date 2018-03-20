f34535845ea8539a8cddce04b1fd2ba4baee89d9
batburger <a_modenbach@hotmail.com>
2018-03-20 19:05:09 +0100

    Implemented interface

--- header/native-backend/widgets/TextWidget.h
+++ header/native-backend/widgets/TextWidget.h
@@ -11,23 +11,26 @@
 namespace nvb{
     /*!\brief Basic Widget which takes no children and shows simple text on screen.*/
     class TextWidget : public IWidget {
-    private:
-        const std::string template_html = "<span style=\"white-space: nowrap\">[INSERT]</span>";
-        const std::string generated_html_;
-
-        std::string generateHtml(std::string &text);
-
     public:
+        std::string text;
         explicit TextWidget(std::string& text);
         explicit TextWidget(std::string&& text);
 
         std::string build(std::string& document, size_t pos) override;
         std::string build(std::string&& document, size_t pos) override;
 
+        boost::shared_ptr<IWidget> generateHtml() override;
+        void updateState() override;
         size_t contentSize() override;
+        boost::shared_ptr<IWidget> add(boost::shared_ptr<IWidget> widget) override;
+        boost::shared_ptr<IWidget> setProperty(std::pair<std::string, boost::shared_ptr<void>>) override;
 
         static boost::shared_ptr<IWidget> createShared(std::string& text);
         static boost::shared_ptr<IWidget> createShared(std::string&& text);
+
+    private:
+        const std::string template_html = "<span style=\"white-space: nowrap\">[INSERT]</span>";
+        std::string generated_html_;
 
     };
 }
--- source/native-backend/widgets/TextWidget.cpp
+++ source/native-backend/widgets/TextWidget.cpp
@@ -1,16 +1,17 @@
 #include <native-backend/widgets/TextWidget.h>
+#include <native-backend/errors/errors.h>
 
-nvb::TextWidget::TextWidget(std::string &text) : generated_html_(generateHtml(text)){
-
+nvb::TextWidget::TextWidget(std::string &text) : text(text){
+    generateHtml();
 }
 
-nvb::TextWidget::TextWidget(std::string &&text) : generated_html_(generateHtml(text)) {
-
+nvb::TextWidget::TextWidget(std::string &&text) : text(text){
+    generateHtml();
 }
 
 /*!\brief Injects already generated HTML into the document at \c pos.*/
 std::string nvb::TextWidget::build(std::string &document, size_t pos) {
-        return document.insert(pos, generated_html_);
+    return document.insert(pos, generated_html_);
 }
 /*!\brief Injects already generated HTML into the document at \c pos.*/
 std::string nvb::TextWidget::build(std::string &&document, size_t pos) {
@@ -19,11 +20,15 @@ std::string nvb::TextWidget::build(std::string &&document, size_t pos) {
 
 /*!\brief Returns a \c boost::shared_ptr<IWidget> instance with a newly allocated \c TextWidget.*/
 boost::shared_ptr<nvb::IWidget> nvb::TextWidget::createShared(std::string &text) {
-    return boost::shared_ptr<nvb::IWidget>(new TextWidget(text));
+    auto sPtr = boost::shared_ptr<nvb::IWidget>(new TextWidget(text));
+    sPtr->setSharedPtrToThis(sPtr);
+    return sPtr;
 }
 /*!\brief Returns a \c boost::shared_ptr<IWidget> instance with a newly allocated \c TextWidget.*/
 boost::shared_ptr<nvb::IWidget> nvb::TextWidget::createShared(std::string &&text) {
-    return boost::shared_ptr<nvb::IWidget>(new TextWidget(text));
+    auto sPtr = boost::shared_ptr<nvb::IWidget>(new TextWidget(text));
+    sPtr->setSharedPtrToThis(sPtr);
+    return sPtr;
 }
 
 /*!\brief Returns the size of the content that is going to be injected into the document.*/
@@ -32,10 +37,23 @@ size_t nvb::TextWidget::contentSize() {
 }
 
 /*!\brief Generates the HTML which is going to be injected into the document.*/
-std::string nvb::TextWidget::generateHtml(std::string &text) {
+boost::shared_ptr<nvb::IWidget> nvb::TextWidget::generateHtml() {
     size_t pos_change = template_html.find_first_of("[INSERT]");
     std::string out = template_html;
     out.replace(pos_change, 8, "");
     out.insert(pos_change, text);
-    return out;
+    generated_html_ = out;
+    return getSharedPtrToThis();
+}
+
+boost::shared_ptr<nvb::IWidget> nvb::TextWidget::add(boost::shared_ptr<nvb::IWidget>) {
+    throw error::no_child_error("A TextWidget can't take a child!");
+}
+
+void nvb::TextWidget::updateState() {
+
+}
+
+boost::shared_ptr<nvb::IWidget> nvb::TextWidget::setProperty(std::pair<std::string, boost::shared_ptr<void>> pair) {
+    return getSharedPtrToThis();
 }
