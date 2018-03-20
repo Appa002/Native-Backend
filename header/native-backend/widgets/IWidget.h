804d654084bdfce9d1934fd79f892edd254234a5
batburger <a_modenbach@hotmail.com>
2018-03-20 19:04:30 +0100

    Changed Interface

--- header/native-backend/widgets/IWidget.h
+++ header/native-backend/widgets/IWidget.h
@@ -13,6 +13,10 @@ namespace nvb{
     /*!\brief Interface from which all Widgets inherit.
      * The \c build method is used by all Widgets to inject there html code into the document.*/
     class IWidget {
+    private:
+        boost::shared_ptr<IWidget> parent_ = boost::shared_ptr<IWidget>(nullptr);
+        boost::shared_ptr<IWidget> shared_ptr_to_this = boost::shared_ptr<IWidget>(nullptr);
+
     public:
         virtual ~IWidget() = default;
 
@@ -23,6 +27,21 @@ namespace nvb{
         /*!\brief Returns the size of the string which is going to be injected into the document.
          * This size will also include the HTML code of all children.*/
         virtual size_t contentSize() = 0;
+
+        virtual boost::shared_ptr<IWidget> add(boost::shared_ptr<IWidget>) = 0;
+        virtual boost::shared_ptr<IWidget> setProperty(std::pair<std::string, boost::shared_ptr<void>>) = 0;
+
+        virtual void updateState() = 0;
+
+        virtual boost::shared_ptr<IWidget> generateHtml() = 0;
+
+        virtual boost::shared_ptr<IWidget> getParent() { return parent_; };
+
+        virtual void setParent(boost::shared_ptr<IWidget> p) { parent_ = std::move(p); };
+
+        virtual boost::shared_ptr<IWidget> getSharedPtrToThis() { return shared_ptr_to_this; };
+
+        virtual void setSharedPtrToThis(boost::shared_ptr<IWidget> p) { shared_ptr_to_this = std::move(p); };
     };
 }
 
