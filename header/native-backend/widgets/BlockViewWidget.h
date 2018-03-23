5717ac56091720d8b91fddc35afc2f382299fb19
batburger <a_modenbach@hotmail.com>
2018-03-23 17:08:52 +0100

    Added override keyword.

--- header/native-backend/widgets/BlockViewWidget.h
+++ header/native-backend/widgets/BlockViewWidget.h
@@ -46,7 +46,7 @@ namespace nvb{
         std::string build(std::string& document, size_t pos) override;
         std::string build(std::string&& document, size_t pos) override;
         size_t contentSize() override;
-        boost::shared_ptr<IWidget> updateAll();
+        boost::shared_ptr<IWidget> updateAll() override;
         boost::shared_ptr<IWidget> add(boost::shared_ptr<IWidget> widget) override;
         boost::shared_ptr<IWidget> setProperty(std::pair<std::string, boost::shared_ptr<void>>) override;
 
