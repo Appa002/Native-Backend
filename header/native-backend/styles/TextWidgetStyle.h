be5cfcac9740c0793e788cccd1e4f69daaaf6c45
batburger <a_modenbach@hotmail.com>
2018-03-24 12:11:32 +0100

    Split into .h & .cpp

--- header/native-backend/styles/TextWidgetStyle.h
+++ header/native-backend/styles/TextWidgetStyle.h
@@ -14,72 +14,33 @@
 namespace nvb {
     class TextWidgetStyle {
     private:
-        unsigned int font_weight_;
+        unsigned int weight_;
         unsigned long color_;
         UnitValue<unsigned int> size_;
+        std::string font_;
 
-        TextWidgetStyle(unsigned int font_weight, unsigned long font_color, UnitValue<unsigned int> size) :
-                font_weight_(setFontWeight(font_weight)), color_(setColor(font_color)), size_(std::move(size)) {};
+        TextWidgetStyle(unsigned int font_weight, unsigned long font_color, UnitValue<unsigned int> size,
+                        std::string font);
 
     public:
 
-        static boost::shared_ptr<TextWidgetStyle> createShared(unsigned int font_weight = 400,
-                                                               unsigned long font_color = 0x0,
-                                                               UnitValue<unsigned int> font_size =
-                                                                    UnitValue<unsigned int>(1, "em")) {
+        static boost::shared_ptr<TextWidgetStyle>
+        createShared(unsigned int font_weight = 400,
+                     unsigned long font_color = 0x0,
+                     UnitValue<unsigned int> font_size = UnitValue<unsigned int>(1, "em"),
+                     std::string font = R"("Times New Roman", Times, serif)");
 
-            return boost::shared_ptr<TextWidgetStyle>(new TextWidgetStyle(font_weight, font_color, std::move(font_size)));
-        }
 
-        unsigned int setFontWeight(unsigned int weight);
+        unsigned int setWeight(unsigned int weight);
+        unsigned int getWeight();
+
         size_t setColor(unsigned long color);
+        size_t getColor();
+
+        UnitValue<unsigned int> getSize();
         UnitValue<unsigned int> setSize(UnitValue<unsigned int> size);
 
-        unsigned int getFontWeight();
-        size_t getColor();
-        UnitValue<unsigned int> getSize();
-
     };
-}
-
-unsigned int nvb::TextWidgetStyle::setFontWeight(unsigned int weight) {
-    if (weight % 100 == 0 && weight >= 100 && weight <= 900) {
-        font_weight_ = weight;
-        return weight;
-    } else{
-        throw error::invalid_argument_error("The font weight must either be 100, 200, 300, 400, 500, 600, 700, 800 or 900. Requested weight: " + std::to_string(weight));
-    }
-
-}
-
-size_t nvb::TextWidgetStyle::setColor(unsigned long color) {
-    if((color >> 24) == 0){
-        color_ = color;
-        return color;
-    }else{
-        std::stringstream stream;
-        stream << "0x" << std::hex << color;
-        std::string colorAsHex( stream.str() );
-        throw error::invalid_argument_error("The Color must be in the range of 0x0 to 0xffffff. Requested color: " + colorAsHex);
-
-    }
-}
-
-nvb::UnitValue<unsigned int> nvb::TextWidgetStyle::setSize(UnitValue<unsigned int> size) {
-    size_ = size;
-    return size;
-}
-
-unsigned int nvb::TextWidgetStyle::getFontWeight() {
-    return font_weight_;
-}
-
-size_t nvb::TextWidgetStyle::getColor() {
-    return color_;
-}
-
-nvb::UnitValue<unsigned int> nvb::TextWidgetStyle::getSize() {
-    return size_;
 }
 
 
---
+++ source/native-backend/styles/TextWidgetStyle.cpp
@@ -0,0 +1,63 @@
+#include <native-backend/styles/TextWidgetStyle.h>
+
+nvb::TextWidgetStyle::TextWidgetStyle(unsigned int font_weight, unsigned long font_color,
+                                      nvb::UnitValue<unsigned int> size, std::string font) :
+        weight_(setWeight(font_weight)), color_(setColor(font_color)),
+        size_(std::move(size)), font_(std::move(font)) {}
+
+
+
+unsigned int nvb::TextWidgetStyle::setWeight(unsigned int weight) {
+    if (weight % 100 == 0 && weight >= 100 && weight <= 900) {
+        weight_ = weight;
+        return weight;
+    } else {
+        throw error::invalid_argument_error(
+                "The font weight must either be 100, 200, 300, 400, 500, 600, 700, 800 or 900. Requested weight: " +
+                std::to_string(weight));
+    }
+
+}
+
+size_t nvb::TextWidgetStyle::setColor(unsigned long color) {
+    if ((color >> 24) == 0) {
+        color_ = color;
+        return color;
+    } else {
+        std::stringstream stream;
+        stream << "0x" << std::hex << color;
+        std::string colorAsHex(stream.str());
+        throw error::invalid_argument_error(
+                "The Color must be in the range of 0x0 to 0xffffff. Requested color: " + colorAsHex);
+
+    }
+}
+
+nvb::UnitValue<unsigned int> nvb::TextWidgetStyle::setSize(UnitValue<unsigned int> size) {
+    size_ = size;
+    return size;
+}
+
+unsigned int nvb::TextWidgetStyle::getWeight() {
+    return weight_;
+}
+
+size_t nvb::TextWidgetStyle::getColor() {
+    return color_;
+}
+
+nvb::UnitValue<unsigned int> nvb::TextWidgetStyle::getSize() {
+    return size_;
+}
+
+
+boost::shared_ptr<nvb::TextWidgetStyle>
+nvb::TextWidgetStyle::createShared(unsigned int font_weight, unsigned long font_color,
+                                   nvb::UnitValue<unsigned int> font_size, std::string font) {
+
+
+    return boost::shared_ptr<TextWidgetStyle>(
+            new TextWidgetStyle(font_weight, font_color, std::move(font_size), std::move(font)));
+
+
+};
