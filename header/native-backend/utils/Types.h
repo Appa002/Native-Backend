819c9fc76af987c233529ed5033171de7d4cfbe5
batburger <a_modenbach@outlook.com>
2018-04-22 09:31:10 +0200

    Added nvb::string_view for use with literals

--- header/native-backend/utils/Types.h
+++ header/native-backend/utils/Types.h
@@ -6,6 +6,7 @@
 #define NATIVE_BACKEND_TYPES_H
 
 #include <string>
+#include <cstring>
 
 namespace nvb{
     template <class T>
@@ -16,6 +17,18 @@ namespace nvb{
         UnitValue(T val, std::string& unit) : value(val), unit(unit) {}
         UnitValue(T val, std::string&& unit) : value(val), unit(unit) {}
     };
+
+    class string_view {
+    public:
+        constexpr string_view(const char* string) : string(string), length(std::strlen(string)) {}
+        constexpr string_view(const char* string, const size_t length) : string(string), length(length) {}
+
+        const char* string;
+        const size_t length;
+
+        operator std::string() const { return string; }
+    };
+
 }
 
 #endif //NATIVE_BACKEND_TYPES_H
